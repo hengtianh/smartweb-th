d4bfb94beadb6a75b58e7efb69836b55c7cf139e
hengtianh <tianhengyanzheng@163.com>
2018-09-23 14:26:23 +0800

    remove unuserd import

--- src/main/java/org/smartweb/aop/TimeAspect.java
+++ src/main/java/org/smartweb/aop/TimeAspect.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smartweb.Annotation.Aspect;
 import org.smartweb.Annotation.Service;
-
 import java.lang.reflect.Method;
 
 @Aspect(Service.class)
